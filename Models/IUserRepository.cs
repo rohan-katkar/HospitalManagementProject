251983acef6cbc87c7b0a5f095e63db4578823cd
Rohan Katkar <rohan.katkar@outlook.com>
2022-12-31 16:36:55 +0530

    Remove unnecessary values

--- Models/IUserRepository.cs
+++ Models/IUserRepository.cs
@@ -1,11 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata;
-
-namespace HospitalManagement.Models
+﻿namespace HospitalManagement.Models
 {
     public interface IUserRepository
     {
         string RepositoryName { get; }
-
         public List<User> GetAllUsers();
         public User GetUser(int id);
     }
